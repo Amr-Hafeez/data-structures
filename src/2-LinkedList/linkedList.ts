1dd13117852dffb20b056275b7dce2f291ba25a7
Amr Khalid <Amr.Hafeez02@eng-st.cu.edu.eg>
2024-03-23 10:53:18 +0200

    Finished the first 6 lectures (until singly LinkedList)

---
+++ src/2-LinkedList/linkedList.ts
@@ -0,0 +1,156 @@
+/*
+* Infrastructure: Non-consecutive memory stores
+*
+*/
+
+
+
+class LinkedListNode {
+    data: number;
+    next: null | LinkedListNode;
+
+    constructor(data: number) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedListIterator {
+    currentNode: LinkedListNode | null | undefined;
+
+    constructor(node: LinkedListNode) {
+        this.currentNode = node;
+    }
+
+    data() {
+        return this.currentNode?.data;
+    }
+
+    next() {
+
+        this.currentNode = this.currentNode?.next;
+        return this;
+    }
+
+    current(): LinkedListNode {
+        return <LinkedListNode>this.currentNode;
+    }
+}
+
+class LinkedList {
+    head: LinkedListNode | null;
+    tail: LinkedListNode | null ;
+
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    begin(): LinkedListIterator {
+        // @ts-ignore
+        return new LinkedListIterator(this.head);
+    }
+
+    printList(): void {
+        for (let itr:LinkedListIterator = this.begin(); itr.current() !== null; itr.next()) {
+            console.log(itr.data(), "->");
+        }
+    }
+
+    find(data: number) {
+        for (let itr:LinkedListIterator = this.begin(); itr.current() !== null; itr.next()) {
+            if (data == itr.data()) {
+                return itr.current();
+            }
+        }
+    }
+
+    findParent(node: LinkedListNode) {
+        for (let itr: LinkedListIterator = this.begin(); itr.current() !== null; itr.next()) {
+            if (itr.current().next === node) {
+                return itr.current();
+            }
+        }
+    }
+
+    insertLast(data: number): void {
+        const newNode: LinkedListNode = new LinkedListNode(data);
+        if (this.head == null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            // @ts-ignore
+            this.tail?.next = newNode;
+            this.tail = newNode;
+        }
+    }
+
+    insertAfter(node: LinkedListNode, data: number): void {
+        const newNode: LinkedListNode = new LinkedListNode(data);
+        // Think about it!
+        // newNode.next = node.next; // This Line From the Lec.
+        if (node.next === null) {
+            this.tail = newNode;
+        } else {
+            newNode.next = node.next;
+        }
+        node.next = newNode;
+    }
+
+    insertBefore(node: LinkedListNode, data: number): void {
+        const newNode: LinkedListNode = new LinkedListNode(data);
+        newNode.next = node;
+
+        const parent = this.findParent(node);
+        if (parent === null) {
+            this.head = newNode;
+        } else {
+            // @ts-ignore
+            parent.next = newNode;
+        }
+    }
+
+    deleteNode(node: LinkedListNode): void {
+        if (this.head === this.tail) {
+            this.head = null;
+            this.tail = null;
+        } else if (this.head === node) {
+            this.head = node.next;
+        } else {
+            const parent = this.findParent(node);
+            if (this.tail === node) {
+                // @ts-ignore
+                this.tail = parent;
+            } else {
+                // @ts-ignore
+                parent.next = node.next;
+            }
+        }
+    }
+}
+
+
+const linkedList: LinkedList = new LinkedList();
+
+linkedList.insertLast(1);
+linkedList.insertLast(2);
+linkedList.insertLast(3);
+linkedList.insertLast(4);
+
+// linkedList.printList();
+
+// @ts-ignore
+linkedList.insertAfter(linkedList.find(2), 98);
+
+// @ts-ignore
+linkedList.insertBefore(linkedList.find(98), 76);
+
+// @ts-ignore
+linkedList.deleteNode(linkedList.find(98));
+
+// @ts-ignore
+linkedList.deleteNode(linkedList.find(1));
+
+
+linkedList.printList();
+
